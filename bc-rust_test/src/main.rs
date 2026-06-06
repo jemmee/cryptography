d27fb10f5e513954c24ab8b65134dfad58d22fe8
Jemmee Yung <jemmee@gmail.com>
2026-06-05 18:17:54 -0700

    feat: Add implementation in Rust to demonstrate the use of Bouncy Castle Crypto Package For Rust.

--- bc-rust_test/src/main.rs
+++ bc-rust_test/src/main.rs
@@ -5,6 +5,7 @@
 // cargo update
 // cargo run
 
+use bouncycastle_hex as hex;
 use bouncycastle_core::traits::Hash;
 use bouncycastle_sha2 as sha2;
 use bouncycastle_sha3 as sha3;
@@ -13,24 +14,42 @@ use bouncycastle_rng as rng;
 use bouncycastle_hmac::HMAC_SHA256;
 use bouncycastle_core::traits::MAC;
 use bouncycastle_core::key_material::{KeyMaterial256, KeyType};
+use bouncycastle_base64 as base64;
+use bouncycastle_core::traits::KDF;
+use bouncycastle_hkdf::HKDF_SHA256;
 
 fn main() {
     let data: &[u8] = b"Let nothing be done through strife or vainglory; but in lowliness of mind let each esteem other better than themselves.";
+    println!("Data: {}", String::from_utf8_lossy(data));
+
+    let output = hex::encode(data);
+    println!("Hex: {}", output);
+
+    let output = base64::encode(data);
+    println!("Base64: {}", output);
 
     let output: Vec<u8> = sha2::SHA256::new().hash(data);
-    println!("SHA256: {}", hex::encode(output));
+    println!("SHA256 in hex: {}", hex::encode(output));
 
     let output: Vec<u8> = sha3::SHA3_256::new().hash(data);
-    println!("SHA3_256: {}", hex::encode(output));
+    println!("SHA3_256 in hex: {}", hex::encode(output));
 
-    let random_bytes = rng::DefaultRNG::default().next_bytes(32);
-    println!("Random bytes: {:x?}", random_bytes);
+    let random_bytes = rng::DefaultRNG::default().next_bytes(32).unwrap();
+    // println!("Random bytes: {:?}", random_bytes);
+    println!("Random bytes in hex: {}", hex::encode(random_bytes));
 
     let key = KeyMaterial256::from_bytes_as_type(
-            b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f",
-            KeyType::MACKey).unwrap();
+                b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f",
+                KeyType::Seed).unwrap();
+    let hkdf = HKDF_SHA256::new();
+    let derived_key = hkdf.derive_key(&key, b"extra input").unwrap();
+    // println!("Drived key: {:?}", derived_key.ref_to_bytes());
+    println!("Derived key in hex: {}", hex::encode(derived_key.ref_to_bytes()));
 
+    let key = KeyMaterial256::from_bytes_as_type(
+                b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f",
+                KeyType::MACKey).unwrap();
     let hmac = HMAC_SHA256::new(&key).expect("Should succeed because key is long enough and tagged KeyType::MACKey");
     let output: Vec<u8> = hmac.mac(data);
-    println!("HMAC_SHA256: {}", hex::encode(output));
+    println!("HMAC_SHA256 in hex: {}", hex::encode(output));
 }
